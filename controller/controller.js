005c24e0d48d625147bf63967b0badf4e90cc532
Tom Lovett <tomlovett6@gmail.com>
2016-02-26 16:22:20 -0700

    farming out sub-processes on the server

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,39 @@
 var googleTranslate = require('google-translate')('AIzaSyCXxjsWkyGpQxo4WZjWVxi-734NYycQB4M')
 
+
 var simpleTranslate = function(req, res) {
 	var text = req.body
 	console.log('text: ', text)
-	googleTranslate.translate(text.word, text.from, text.to, function(err, translation) {
+	res.send(apiTranslate(text.word, text. from, to))
+}
+
+var apiTranslate = function(word, from, to) {
+	googleTranslate.translate(word, from, to, function(err, translation) {
 		console.log('err: ', err)
 		console.log('translation: ', translation)
-		res.send(translation)
-	})
+		if (err) { return err } 
+		else     { return translation}
+	}) 
+}
+
+// getCorrect
+// storeOne
+// store to
+
+var quizTranslate = function(req, res) {
+	// passing full objects back and forth
+	// {word, from, to, answer, is}
+	// check DB
+	// else, translate through API
+	googleTranslate.translate, 
+}
+
+var pullFromDatabase = function(db, word, from, )
+
+
+var storeToDatabase = function(db, word, into, is) {
+	// load db
+	// store {into, is}
 }
 
 module.exports = {
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -27,7 +27,10 @@ function quizMode($scope, $http) {
 
 	$scope.submitAnswer = function() {
 		// check answer: $scope.answer
-			// check database
+		// question, answer, 'en'
+		// word, from, to
+		// check database
+
 			// get answer from $http post
 		// set true/false response
 
@@ -36,14 +39,30 @@ function quizMode($scope, $http) {
 	var getCorrect = function() {
 		// check database first
 			// return
+
+		$http.post('/api/translate', {
+			word : $scope.question,
+			from : $scope.lang,
+			to   : 'en'
+		}).then(function(returnData) {
+			$scope.correctAnswer = returnData.data
+		})
+		$http.post('/api/store', {
+			word : $scope.question,
+			db : $scope.lang,
+			to   : ''
+			is  : $scope.correctAnswer
+
+		})
 		// call to server,
 			// question, from, to
 		// store to database
 	}
 
 	$scope.nextQuestion = function() {
-		$scope.correct = false
-		$scope.incorrect = false
+		$scope.isCorrect     = false
+		$scope.isIncorrect   = false
+		$scope.correctAnswer = null
 		// call next
 		$liveQuestion = "set me"
 	}
