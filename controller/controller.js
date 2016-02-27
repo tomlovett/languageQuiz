a66eca9c4eb1fe215d82a33dabb0ab659a6206d1
Tom Lovett <tomlovett6@gmail.com>
2016-02-26 17:54:56 -0700

    reconfiguring data structure on the back-end

--- controller/controller.js
+++ controller/controller.js
@@ -1,39 +1,61 @@
 var googleTranslate = require('google-translate')('AIzaSyCXxjsWkyGpQxo4WZjWVxi-734NYycQB4M')
 
-
-var simpleTranslate = function(req, res) {
-	var text = req.body
-	console.log('text: ', text)
-	res.send(apiTranslate(text.word, text. from, to))
+// passing wordPair back and forth
+var wordpair = {
+	langA : '',
+	langB : '',
+	wordA : '',
+	wordB : ''
 }
 
-var apiTranslate = function(word, from, to) {
+var loadNextQuestion = function(req, res) {
+	req.body.question = callRandomWord()
+	// does not account for API failure
+		// if failed would pull random word from db
+			// if db empty, well...
+	req.body.answer = pullFromDatabase(req.body)
+	if (req.body.answer) {
+		res.send(req.body)
+	} else {
+		req.body.answer = apiTranslate()
+	}
+	if (req.body.answer) { 		// restarts search on API failure
+		storeToDatabase(req.body)
+		res.send(req.body)
+	} else {
+		loadNextQuestion(req, res) 
+	}
+}
+
+var callRandomWord = function() {
+	return 'randomWordAPI'
+
+}
+
+var pullFromDatabase = function(wordPair) {
+	// a = look (word, langA, langB)
+	// b =look (word, langB, langA)
+	// if (a) return a
+	// else return b
+	return
+	// the biggest pain point, contemplating using another data structure, or simply accepting that this function will be ugly
+}
+
+var apiTranslate = function(wordPair) {
+	var wordPair = quest
 	googleTranslate.translate(word, from, to, function(err, translation) {
 		console.log('err: ', err)
 		console.log('translation: ', translation)
-		if (err) { return err } 
+		if (err) { return } 
 		else     { return translation}
 	}) 
 }
 
-// getCorrect
-// storeOne
-// store to
-
-var quizTranslate = function(req, res) {
-	// passing full objects back and forth
-	// {word, from, to, answer, is}
-	// check DB
-	// else, translate through API
-	googleTranslate.translate, 
-}
-
-var pullFromDatabase = function(db, word, from, )
-
-
-var storeToDatabase = function(db, word, into, is) {
-	// load db
-	// store {into, is}
+var storeToDatabase = function(wordPair) {
+	// langFrom, langTo, wordFrom, wordTo
+	// storeOne
+	// store to
+	// nested functions?
 }
 
 module.exports = {
